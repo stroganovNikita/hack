c930ee19f75d1ff3aefde81da2500b673ef47565
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-03-31 12:43:44 +0300

    test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser())
+// app.use(cookieParser())
 
 app.post('/', (req, res) => {
-    console.log(req.body)
-    console.log(req.cookies)
+    // console.log(req.body)
+    // console.log(req.cookies)
     console.log(req)
 })
 
