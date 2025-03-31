f4dc0941af2a515cd16f71b465b96c60b80fb2f0
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-03-31 12:49:48 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(cors());
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.post('/', (req, res) => {
     // console.log(req.body)
-    // console.log(req.cookies)
-    console.log(req)
+    console.log(req.cookies)
+    // console.log(req)
 })
 
 app.listen(3000, () => console.log('Server listen on port 3000'))
